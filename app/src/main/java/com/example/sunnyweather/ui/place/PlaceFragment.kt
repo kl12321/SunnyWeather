e5c0d621135427f7637b557ad43f7b07cd6506ce
lenovo <2805617847@qq.com>
2022-08-03 17:27:54 +0800

    实现全球城市搜索功能

---
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.kt
@@ -0,0 +1,72 @@
+package com.example.sunnyweather.ui.place
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import androidx.lifecycle.Observer
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sunnyweather.R
+import kotlinx.android.synthetic.main.fragment_place.*
+
+class PlaceFragment : Fragment() {
+   val viewModel by lazy {
+       ViewModelProvider(this).get(PlaceViewModel::class.java)
+   }
+    private lateinit var adapter:PlaceAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_place, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val layoutManger = LinearLayoutManager(activity)
+        recycleview.layoutManager = layoutManger
+        adapter=PlaceAdapter(this,viewModel.placeList)
+        recycleview.adapter = adapter
+        searchPlaceEdit.addTextChangedListener{ editable->
+            val content = editable.toString()
+            if (content.isNotEmpty()){
+                viewModel.searchPlaces(content)
+            }else{
+                recycleview.visibility=View.GONE
+                bgimageview.visibility= View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+        viewModel.placeLiveData.observe(viewLifecycleOwner,Observer{
+            result ->val places =result.getOrNull()
+            if (places!=null){
+                recycleview.visibility=View.VISIBLE
+                bgimageview.visibility=View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            }else{
+                Toast.makeText(activity,"未能找到查询地点",Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+
+    }
+
+    companion object {
+
+
+    }
+}
